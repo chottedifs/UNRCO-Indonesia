73ab2ec75cb1638ae94993640e79f8b7f30422cf
chottedifs <husadif.aldi@gmail.com>
2022-08-19 11:25:21 +0700

    Update Menus, Scroll Content

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+if(window.location.hash){
+    let hash = window.location.hash;
+    if($(hash).length) {
+        $('html,body').animate({
+            scrollTop: $(hash).offset().top - 120
+        }, 900, '');
+    }
+}
