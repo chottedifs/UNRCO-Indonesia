fd098648f6f2790f9aa95309909a38dbd6a6e83b
chottedifs <husadif.aldi@gmail.com>
2022-08-22 19:59:52 +0700

    Update Scroll to Top

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,18 @@
+var btn = $('#scroll-up');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 if(window.location.hash){
     let hash = window.location.hash;
     if($(hash).length) {
